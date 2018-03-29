198b13d6d6e5321ce836e86dab15cb07242107f6
Adam Deeley <ad.deeley@gmail.com>
2018-03-29 18:36:35 +0100

    Blocks are now added form JSON dynamically.

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,31 @@
 import '../css/main/index.scss';
 import 'normalize.css';
 import {addListeners} from './block-animation';
+import data from '../data/projects.json';
 
-//window.onload = addListeners;
+console.log(data);
+window.onload = () => {
+    const container = document.getElementById('projects_container');
+    const projects = data['projects'];
+    for (let i = 0; i < projects.length; i++) {
+        let project = projects[i];
+        let block = document.createElement('div');
+            block.classList = 'block block--simon';
+        let header = document.createElement('h4');
+            header.classList = 'block__header';
+            header.innerHTML = project['title'];
+        let copy = document.createElement('p');
+            copy.classList = 'block__copy';
+            copy.innerHTML = project['copy'];
+        let button = document.createElement('button');
+            button.classList = 'btn';
+            button.href = project['link'];
+            button.innerHTML = 'Link to project';
+    
+
+        block.appendChild(header);
+        block.appendChild(copy);
+        block.appendChild(button);
+        container.appendChild(block);
+    }
+};
